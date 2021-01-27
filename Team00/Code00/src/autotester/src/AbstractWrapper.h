dc6cc00ce1fd29800e08d6d0758d421a986a0f0f
Kevin Putera <kevintiosanna@gmail.com>
2021-01-27 10:30:45 +0800

    Integrate SIMPLE parser with autotester

--- Team00/Code00/src/autotester/src/AbstractWrapper.h
+++ Team00/Code00/src/autotester/src/AbstractWrapper.h
@@ -7,25 +7,30 @@
 
 class AbstractWrapper {
 public:
-  static volatile bool GlobalStop;
-  // method for parsing the SIMPLE source
-  virtual void parse(std::string filename) = 0;
-  // method for evaluating a query
-  virtual void evaluate(std::string query, std::list<std::string>& results) =0;
+    static volatile bool GlobalStop;
+
+    // method for parsing the SIMPLE source
+    virtual void parse(std::string filename) = 0;
+
+    // method for evaluating a query
+    virtual void evaluate(std::string query, std::list<std::string> &results) = 0;
 };
 
 class WrapperFactory {
 public:
-  // static method for creating a wrapper
-  static AbstractWrapper* createWrapper();
-private:
-  // the following three will prevent the creation of WrapperFactory objects
-  WrapperFactory();
-  WrapperFactory(const WrapperFactory&);
-  WrapperFactory& operator=(const WrapperFactory&);
+    // static method for creating a wrapper
+    static AbstractWrapper *createWrapper();
 
-  // the Wrapper object
-  static AbstractWrapper* wrapper;
+private:
+    // the following three will prevent the creation of WrapperFactory objects
+    WrapperFactory();
+
+    WrapperFactory(const WrapperFactory &);
+
+    WrapperFactory &operator=(const WrapperFactory &);
+
+    // the Wrapper object
+    static AbstractWrapper *wrapper;
 };
 
 #endif
