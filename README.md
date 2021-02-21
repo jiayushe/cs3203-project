962ccc00551979d9cbb1c0feb21e34b4aac762e7
Kevin Putera <kevintiosanna@gmail.com>
2021-02-21 19:38:24 +0800

    Follow submission requirements (#83)
    * Update Contact.csv
    
    * Rename test files to fit submission requirement
    
    * Add build + run instruction for bash on win
    
    * Add more contacts
    
    * Update misc info
    
    * Fix autotest.ps1 bug
    
    * Update Readme.txt
    
    * Update Readme.txt
    
    * Final changes
    
    Co-authored-by: SHUI YAO <46176136+shuiyao-sg@users.noreply.github.com>

--- README.md
+++ README.md
@@ -7,9 +7,3 @@
 ```bash
 make format
 ```
-
-- Run test suites from PowerShell.
-```ps
-# You may need to run `Set-ExecutionPolicy Unrestricted` first to enable execution of remote scripts.
-& .\autotest.ps1
-```
--- autotest.ps1
+++
@@ -1,7 +0,0 @@
-$cnt=[System.IO.Directory]::GetFiles("$pwd\Team28\Tests28\", "*.source.txt").Count
-Write-Host "$cnt test suites found."
-For ($i=1; $i -le $cnt; $i++) {
-    Write-Host "Test suite $i running..."
-    Invoke-Expression "& `".\Team28\Code28\build_win\x86-Release\src\autotester\autotester.exe`" .\Team28\Tests28\$i.source.txt .\Team28\Tests28\$i.queries.txt .\Team28\Tests28\$i.out.xml | out-null"
-    Write-Host "Test suite $i completed."
-}
